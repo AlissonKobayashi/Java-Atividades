88fb149235d4d3321668d14a88ec725b82523566
AlissonKobayashi <134564328+AlissonKobayashi@users.noreply.github.com>
2025-03-03 15:01:42 -0300

    Update ModelFuncionario.java

--- ATIVIDADE_GERENCIADORFUNCIONARIO/gerenciarFuncionarios/src/main/java/com/example/gerenciarFuncionarios/model/domain/ModelFuncionario.java
+++ ATIVIDADE_GERENCIADORFUNCIONARIO/gerenciarFuncionarios/src/main/java/com/example/gerenciarFuncionarios/model/domain/ModelFuncionario.java
@@ -12,6 +12,12 @@ public class ModelFuncionario {
     private String nome;
     private double salario;
 
+    public ModelFuncionario(int id, String nome, double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.salario = salario;
+    }
+
     public int getId() {
         return id;
     }
