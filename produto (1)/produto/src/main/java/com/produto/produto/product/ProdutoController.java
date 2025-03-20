54fe12ee1357bd7b3a45576d28a3d9feb1ad5b17
AlissonKobayashi <134564328+AlissonKobayashi@users.noreply.github.com>
2025-03-19 22:12:28 -0300

    Add files via upload

--- produto (1)/produto/src/main/java/com/produto/produto/product/ProdutoController.java
+++ produto (1)/produto/src/main/java/com/produto/produto/product/ProdutoController.java
@@ -1,13 +1,11 @@
 package com.produto.produto.product;
 
-
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @RestController
 @RequestMapping(path = "/produto")
@@ -17,25 +15,25 @@ public class ProdutoController {
     private ProdutoService produtoService;
 
     @GetMapping
-    private ResponseEntity<List<ProdutoModel>> findAll(){
+    public ResponseEntity<List<ProdutoModel>> findAll() {
         List<ProdutoModel> produtoModel = produtoService.findAll();
         return ResponseEntity.ok().body(produtoModel);
     }
 
     @PostMapping
-    private ResponseEntity<ProdutoModel> criarProduto(@RequestBody ProdutoModel novoProduto){
+    public ResponseEntity<ProdutoModel> criarProduto(@Valid @RequestBody ProdutoModel novoProduto) {
         ProdutoModel produtoModel = produtoService.criarProduto(novoProduto);
         return ResponseEntity.ok().body(produtoModel);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody ProdutoModel editLivro) {
-        produtoService.editarProduto(id, editLivro);
+    public ResponseEntity<Void> update(@PathVariable Long id, @Valid @RequestBody ProdutoModel editProduto) {
+        produtoService.editarProduto(id, editProduto);
         return ResponseEntity.noContent().build();
-}
+    }
 
     @DeleteMapping("/{id}")
-    private ResponseEntity<?> deletarPorId(@PathVariable Long id){
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long id) {
         produtoService.deletarProduto(id);
         return ResponseEntity.noContent().build();
     }
--- produto (1)/produto/src/main/java/com/produto/produto/product/ProdutoModel.java
+++ produto (1)/produto/src/main/java/com/produto/produto/product/ProdutoModel.java
@@ -1,6 +1,8 @@
 package com.produto.produto.product;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 @Table(name = "TBL_PRODUTO")
@@ -10,7 +12,11 @@ public class ProdutoModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @Positive(message = "O preço deve ser maior que zero")
     private Double preco;
+
+    @Min(value = 0, message = "A quantidade em estoque não pode ser menor que zero")
     private Integer quantEmEstoque;
 
     public ProdutoModel(){}
