5c9bfecd4b6f9e6af67fefe87d4bdb5a2417fd7d
AlissonKobayashi <134564328+AlissonKobayashi@users.noreply.github.com>
2025-06-03 13:31:31 -0300

    Iniciando Projeto Spring Initializir

---
+++ provaPratica-2Bim/provaPraticaEliel/src/test/java/com/example/provaPraticaEliel/ProvaPraticaElielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provaPraticaEliel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaPraticaElielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
