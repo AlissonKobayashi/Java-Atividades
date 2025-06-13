da206fd21b8720e11ba540dd10e0a15a126395c4
AlissonKobayashi <134564328+AlissonKobayashi@users.noreply.github.com>
2025-06-13 13:06:54 -0300

    feat: Criação do token

---
+++ ProvaPratica-2BIM/provaPraticaEliel/src/main/java/com/example/provaPraticaEliel/usuario/TokenResponse.java
@@ -0,0 +1,14 @@
+package com.example.provaPraticaEliel.usuario;
+
+public class TokenResponse {
+
+    private final String token;
+
+    public TokenResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
